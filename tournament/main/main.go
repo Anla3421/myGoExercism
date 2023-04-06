d8cbc5981a1e9e8c6b9e1a7eb454b0774e713558
jared.liu <jared.liu@grandsys.com>
2023-04-06 17:33:54 +0800

    update tournament

--- tournament/main/main.go
+++ tournament/main/main.go
@@ -11,11 +11,17 @@ func main() {
 	var writer io.Writer
 	var input io.Reader
 	input = strings.NewReader(`Allegoric Alaskians;Blithering Badgers;win
-	Devastating Donkeys;Courageous Californians;draw
-	Devastating Donkeys;Allegoric Alaskians;win
-	Courageous Californians;Blithering Badgers;loss
-	Blithering Badgers;Devastating Donkeys;loss
-	Allegoric Alaskians;Courageous Californians;win`)
+Devastating Donkeys;Courageous Californians;draw
+Devastating Donkeys;Allegoric Alaskians;win
+Courageous Californians;Blithering Badgers;loss
+Blithering Badgers;Devastating Donkeys;loss
+Allegoric Alaskians;Courageous Californians;win
+`)
+
+	// 	input = strings.NewReader(`
+	// Allegoric Alaskians;Blithering Badgers;win
+	// Allegoric Alaskians;Courageous Californians;win
+	// `)
 
 	result := tournament.Tally(input, writer)
 	fmt.Println("result", result)
--- tournament/tournament.go
+++ tournament/tournament.go
@@ -8,6 +8,13 @@ import (
 
 func Tally(reader io.Reader, writer io.Writer) error {
 	p := make([]byte, 1024)
+	fileTable := map[string][]int{
+		"Devastating Donkeys":     {0, 0, 0},
+		"Allegoric Alaskians":     {0, 0, 0},
+		"Blithering Badgers":      {0, 0, 0},
+		"Courageous Californians": {0, 0, 0},
+	}
+
 	for {
 		n, err := reader.Read(p)
 		if err == io.EOF {
@@ -16,8 +23,62 @@ func Tally(reader io.Reader, writer io.Writer) error {
 		}
 		// fmt.Println(n, string(p[:n]))
 	}
-	fmt.Println("this is P", string(p))
+	// fmt.Println("this is P", string(p))
 	tempSlice := strings.Split(string(p), "\n")
-	fmt.Println("tempSlice 0", tempSlice[0])
+	for _, v := range tempSlice {
+		mp := 1
+		w := 1
+		d := 1
+		tempSlice2 := strings.Split(v, ";")
+		if len(tempSlice2) == 3 {
+			switch tempSlice2[2] {
+			case "win":
+				fileTable[tempSlice2[0]][0] = fileTable[tempSlice2[0]][0] + mp
+				fileTable[tempSlice2[0]][1] = fileTable[tempSlice2[0]][1] + w
+
+				fileTable[tempSlice2[1]][0] = fileTable[tempSlice2[1]][0] + mp
+			case "loss":
+				fileTable[tempSlice2[0]][0] = fileTable[tempSlice2[0]][0] + mp
+
+				fileTable[tempSlice2[1]][0] = fileTable[tempSlice2[1]][0] + mp
+				fileTable[tempSlice2[1]][1] = fileTable[tempSlice2[1]][1] + w
+			case "draw":
+				fileTable[tempSlice2[0]][0] = fileTable[tempSlice2[0]][0] + mp
+				fileTable[tempSlice2[0]][2] = fileTable[tempSlice2[0]][2] + d
+
+				fileTable[tempSlice2[1]][0] = fileTable[tempSlice2[1]][0] + mp
+				fileTable[tempSlice2[1]][2] = fileTable[tempSlice2[1]][2] + d
+			}
+		}
+	}
+
+	teamDD := []int{
+		fileTable["Devastating Donkeys"][0], fileTable["Devastating Donkeys"][1], fileTable["Devastating Donkeys"][2], fileTable["Devastating Donkeys"][0] - fileTable["Devastating Donkeys"][1] - fileTable["Devastating Donkeys"][2], fileTable["Devastating Donkeys"][1]*3 + fileTable["Devastating Donkeys"][2],
+	}
+	teamAA := []int{
+		fileTable["Allegoric Alaskians"][0], fileTable["Allegoric Alaskians"][1], fileTable["Allegoric Alaskians"][2], fileTable["Allegoric Alaskians"][0] - fileTable["Allegoric Alaskians"][1] - fileTable["Allegoric Alaskians"][2], fileTable["Allegoric Alaskians"][1]*3 + fileTable["Allegoric Alaskians"][2],
+	}
+	teamBB := []int{
+		fileTable["Blithering Badgers"][0], fileTable["Blithering Badgers"][1], fileTable["Blithering Badgers"][2], fileTable["Blithering Badgers"][0] - fileTable["Blithering Badgers"][1] - fileTable["Blithering Badgers"][2], fileTable["Blithering Badgers"][1]*3 + fileTable["Blithering Badgers"][2],
+	}
+	teamCC := []int{
+		fileTable["Courageous Californians"][0], fileTable["Courageous Californians"][1], fileTable["Courageous Californians"][2], fileTable["Courageous Californians"][0] - fileTable["Courageous Californians"][1] - fileTable["Courageous Californians"][2], fileTable["Courageous Californians"][1]*3 + fileTable["Courageous Californians"][2],
+	}
+	result := fmt.Sprintf(`
+Team                           | MP |  W |  D |  L |  P
+Devastating Donkeys            |  %v |  %v |  %v |  %v |  %v
+Allegoric Alaskians            |  %v |  %v |  %v |  %v |  %v
+Blithering Badgers             |  %v |  %v |  %v |  %v |  %v
+Courageous Californians        |  %v |  %v |  %v |  %v |  %v
+`, teamDD[0], teamDD[1], teamDD[2], teamDD[3], teamDD[4],
+		teamAA[0], teamAA[1], teamAA[2], teamAA[3], teamAA[4],
+		teamBB[0], teamBB[1], teamBB[2], teamBB[3], teamBB[4],
+		teamCC[0], teamCC[1], teamCC[2], teamCC[3], teamCC[4],
+	)
+	fmt.Println("result", result)
+	n, err := writer.Write([]byte(result))
+	if err == io.EOF {
+		fmt.Printf("n: %d, err: %v\n", n, err)
+	}
 	return nil
 }
