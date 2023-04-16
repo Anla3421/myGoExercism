fbb4e2ec50e5ac3a98d74fcee63ff488bfce32cf
jared.liu <jared.liu@grandsys.com>
2023-04-17 00:59:48 +0800

    update tournament

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -4,17 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"sort"
 	"strings"
 )
 
 func Tally(reader io.Reader, writer io.Writer) error {
 	p := make([]byte, 1024)
-	// fileTable := map[string][]int{
-	// 	"Devastating Donkeys":     {0, 0, 0},
-	// 	"Allegoric Alaskians":     {0, 0, 0},
-	// 	"Blithering Badgers":      {0, 0, 0},
-	// 	"Courageous Californians": {0, 0, 0},
-	// }
 
 	for {
 		n, err := reader.Read(p)
@@ -22,37 +17,7 @@ func Tally(reader io.Reader, writer io.Writer) error {
 			fmt.Printf("n: %d, err: %v\n", n, err)
 			break
 		}
-		// fmt.Println(n, string(p[:n]))
 	}
-	// fmt.Println("this is P", string(p))
-
-	// tempSlice := strings.Split(string(p), "\n")
-	// for _, v := range tempSlice {
-	// 	mp := 1
-	// 	w := 1
-	// 	d := 1
-	// 	tempSlice2 := strings.Split(v, ";")
-	// 	if len(tempSlice2) == 3 {
-	// 		switch tempSlice2[2] {
-	// 		case "win":
-	// 			fileTable[tempSlice2[0]][0] = fileTable[tempSlice2[0]][0] + mp
-	// 			fileTable[tempSlice2[0]][1] = fileTable[tempSlice2[0]][1] + w
-
-	// 			fileTable[tempSlice2[1]][0] = fileTable[tempSlice2[1]][0] + mp
-	// 		case "loss":
-	// 			fileTable[tempSlice2[0]][0] = fileTable[tempSlice2[0]][0] + mp
-
-	// 			fileTable[tempSlice2[1]][0] = fileTable[tempSlice2[1]][0] + mp
-	// 			fileTable[tempSlice2[1]][1] = fileTable[tempSlice2[1]][1] + w
-	// 		case "draw":
-	// 			fileTable[tempSlice2[0]][0] = fileTable[tempSlice2[0]][0] + mp
-	// 			fileTable[tempSlice2[0]][2] = fileTable[tempSlice2[0]][2] + d
-
-	// 			fileTable[tempSlice2[1]][0] = fileTable[tempSlice2[1]][0] + mp
-	// 			fileTable[tempSlice2[1]][2] = fileTable[tempSlice2[1]][2] + d
-	// 		}
-	// 	}
-	// }
 
 	fileTable := Calculate(string(p))
 
@@ -60,6 +25,22 @@ func Tally(reader io.Reader, writer io.Writer) error {
 		return errors.New("teams error")
 	}
 
+	tempTable := map[int]int{}
+	for _, v := range fileTable {
+		tempTable[v[5]] = v[4]
+	}
+	fmt.Println("tempTable", tempTable)
+
+	// 先將所有 key 存入 []string 變數
+	keys := make([]string, 0, len(fileTable))
+	for key := range fileTable {
+		keys = append(keys, key)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		return fileTable[keys[i]][len(fileTable[keys[i]])-2] > fileTable[keys[j]][len(fileTable[keys[j]])-2]
+	})
+
+	fmt.Println("keys", keys)
 	teamDD := []int{
 		fileTable["Devastating Donkeys"][0], fileTable["Devastating Donkeys"][1], fileTable["Devastating Donkeys"][2], fileTable["Devastating Donkeys"][0] - fileTable["Devastating Donkeys"][1] - fileTable["Devastating Donkeys"][2], fileTable["Devastating Donkeys"][1]*3 + fileTable["Devastating Donkeys"][2],
 	}
@@ -72,7 +53,7 @@ func Tally(reader io.Reader, writer io.Writer) error {
 	teamCC := []int{
 		fileTable["Courageous Californians"][0], fileTable["Courageous Californians"][1], fileTable["Courageous Californians"][2], fileTable["Courageous Californians"][0] - fileTable["Courageous Californians"][1] - fileTable["Courageous Californians"][2], fileTable["Courageous Californians"][1]*3 + fileTable["Courageous Californians"][2],
 	}
-	println("1111", teamDD, teamAA, teamBB, teamCC)
+	// println("1111", teamDD, teamAA, teamBB, teamCC)
 
 	result := fmt.Sprintf(`
 	Team                           | MP |  W |  D |  L |  P
@@ -85,7 +66,7 @@ func Tally(reader io.Reader, writer io.Writer) error {
 		teamBB[0], teamBB[1], teamBB[2], teamBB[3], teamBB[4],
 		teamCC[0], teamCC[1], teamCC[2], teamCC[3], teamCC[4],
 	)
-	println("2222", result)
+	// println("2222", result)
 
 	n, err := writer.Write([]byte(result))
 	if err == io.EOF {
@@ -95,11 +76,12 @@ func Tally(reader io.Reader, writer io.Writer) error {
 }
 
 func Calculate(p string) map[string][]int {
-	fileTable := map[string][]int{
-		"Devastating Donkeys":     {0, 0, 0},
-		"Allegoric Alaskians":     {0, 0, 0},
-		"Blithering Badgers":      {0, 0, 0},
-		"Courageous Californians": {0, 0, 0},
+	// mp, win, draw, loss, point
+	tempFileTable := map[string][]int{
+		"Devastating Donkeys":     {0, 0, 0, 0, 0, 4},
+		"Allegoric Alaskians":     {0, 0, 0, 0, 0, 1},
+		"Blithering Badgers":      {0, 0, 0, 0, 0, 2},
+		"Courageous Californians": {0, 0, 0, 0, 0, 3},
 	}
 
 	tempSlice := strings.Split(string(p), "\n")
@@ -111,24 +93,30 @@ func Calculate(p string) map[string][]int {
 		if len(tempSlice2) == 3 {
 			switch tempSlice2[2] {
 			case "win":
-				fileTable[tempSlice2[0]][0] = fileTable[tempSlice2[0]][0] + mp
-				fileTable[tempSlice2[0]][1] = fileTable[tempSlice2[0]][1] + w
+				tempFileTable[tempSlice2[0]][0] = tempFileTable[tempSlice2[0]][0] + mp
+				tempFileTable[tempSlice2[0]][1] = tempFileTable[tempSlice2[0]][1] + w
 
-				fileTable[tempSlice2[1]][0] = fileTable[tempSlice2[1]][0] + mp
+				tempFileTable[tempSlice2[1]][0] = tempFileTable[tempSlice2[1]][0] + mp
 			case "loss":
-				fileTable[tempSlice2[0]][0] = fileTable[tempSlice2[0]][0] + mp
+				tempFileTable[tempSlice2[0]][0] = tempFileTable[tempSlice2[0]][0] + mp
 
-				fileTable[tempSlice2[1]][0] = fileTable[tempSlice2[1]][0] + mp
-				fileTable[tempSlice2[1]][1] = fileTable[tempSlice2[1]][1] + w
+				tempFileTable[tempSlice2[1]][0] = tempFileTable[tempSlice2[1]][0] + mp
+				tempFileTable[tempSlice2[1]][1] = tempFileTable[tempSlice2[1]][1] + w
 			case "draw":
-				fileTable[tempSlice2[0]][0] = fileTable[tempSlice2[0]][0] + mp
-				fileTable[tempSlice2[0]][2] = fileTable[tempSlice2[0]][2] + d
+				tempFileTable[tempSlice2[0]][0] = tempFileTable[tempSlice2[0]][0] + mp
+				tempFileTable[tempSlice2[0]][2] = tempFileTable[tempSlice2[0]][2] + d
 
-				fileTable[tempSlice2[1]][0] = fileTable[tempSlice2[1]][0] + mp
-				fileTable[tempSlice2[1]][2] = fileTable[tempSlice2[1]][2] + d
+				tempFileTable[tempSlice2[1]][0] = tempFileTable[tempSlice2[1]][0] + mp
+				tempFileTable[tempSlice2[1]][2] = tempFileTable[tempSlice2[1]][2] + d
 			}
 		}
 	}
-
+	fileTable := map[string][]int{}
+	// mp, win, draw, loss, point
+	for k, v := range tempFileTable {
+		v[3] = v[0] - v[1] - v[2]
+		v[4] = v[1]*3 + v[2]
+		fileTable[k] = v
+	}
 	return fileTable
 }
